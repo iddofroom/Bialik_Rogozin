2a04bd81959aa0e6dc29661f2c195ed6b05bea51
iddofroom <iddofroom@gmail.com>
2019-02-18 01:56:59 +0200

    Update 3stones.ino

--- 3stones.ino
+++ 3stones.ino
@@ -1,28 +1,10 @@
-/*
- Example using the SparkFun HX711 breakout board with a scale
- By: Nathan Seidle
- SparkFun Electronics
- Date: November 19th, 2014
- License: This code is public domain but you buy me a beer if you use this and we meet someday (Beerware license).
- This example demonstrates basic scale output. See the calibration sketch to get the calibration_factor for your
- specific load cell setup.
- This example code uses bogde's excellent library:  https://github.com/bogde/HX711
 
- bogde's library is released under a GNU GENERAL PUBLIC LICENSE
- The HX711 does one thing well: read load cells. The breakout board is compatible with any wheat-stone bridge
- based load cell which should allow a user to measure everything from a few grams to tens of tons.
- Arduino pin 2 -> HX711 CLK
- 3 -> DAT
- 5V -> VCC
- GND -> GND
- The HX711 board can be powered from 2.7V to 5V so the Arduino 5V power should be fine.
-*/
 #include <Wire.h>
 #include "HX711.h"
 ///////////////////////////////////////
 #define calibration_factor -14050.0 //This value is obtained using the SparkFun_HX711_Calibration sketch
-#define minw  1
-#define maxw  20
+#define minw  0.2   // change this value of cell load have lot of noice
+#define maxw  20 // same as value before
 #define sensors  3
 #define DOUT1 3
 #define CLK1  2
@@ -30,8 +12,8 @@
 #define CLK3  8
 #define DOUT2  11
 #define CLK2  10
-bool stons[9] = {false,false,false,false,false,false,false,false,false};
-float history[9][3] ={(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0)};
+bool stons[3] = {false,false,false};
+float history[9][3] ={(0,0,0),(0,0,0),(0,0,0)};
 HX711 scale1(DOUT1, CLK1);
 HX711 scale2(DOUT2, CLK2);
 HX711 scale3(DOUT3, CLK3);
@@ -44,7 +26,7 @@ void setup() {
   scale2.tare(); //Assuming there is no weight on the scale at start up, reset the scale to 0
   scale3.set_scale(calibration_factor); //This value is obtained by using the SparkFun_HX711_Calibration sketch
   scale3.tare(); //Assuming there is no weight on the scale at start up, reset the scale to 0
-  Wire.begin(7);                // join i2c bus with address #8
+  Wire.begin(7);                // join i2c bus with address #7
   Wire.onRequest(requestEvent); // register event
   history[0][0]=scale1.get_units();
   history[1][0]=scale2.get_units();
@@ -53,22 +35,22 @@ void setup() {
 // List of patterns to cycle through.  Each is defined as a separate function below.
 
 void loop() {
-      for (int k=0;k<sensors;k++)
-   {
-    history[k][2]=history[k][1];
-    history[k][1]=history[k][0];
-    }
+   for (int k=0;k<sensors;k++)
+      {
+      history[k][2]=history[k][1];
+      history[k][1]=history[k][0];
+      }
    history[0][0]=scale1.get_units();
    history[1][0]=scale2.get_units();
    history[2][0]=scale3.get_units();
-  for (int k=0;k<sensors;k++)
+   for (int k=0;k<sensors;k++)
       {Serial.print(history[k][0], 1); Serial.print(" ");} //scale.get_units() returns a float
    Serial.println();
    for (int k=0;k<sensors;k++)
       { stons[k] = false;
         if ( abs(history[k][0]-history[k][1])>minw && abs(history[k][0]-history[k][1])<maxw && abs(history[k][1]-history[k][2])>minw && abs(history[k][1]-history[k][2])<maxw && abs(history[k][0]-history[k][2])>minw && abs(history[k][0]-history[k][2])<maxw )
         {stons[k]=true;Serial.println("-------------------------");}}       
-delay(500);
+delay(100);
 }
 void requestEvent() {
   for (int l=0; l<sensors;l++)
